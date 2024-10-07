05bd4fa6adb70e6ea43dd7c0cdeaa2ab0cd77a4e
Gyuwon Lee (ollie) <94846990+gyutato@users.noreply.github.com>
2024-10-07 11:58:34 +0900

    Time: 88 ms (13.12%), Space: 53.6 MB (46.23%) - LeetHub

---
+++ LeetCode/0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false
+
+    const hashS = {}
+    const hashT = {}
+
+    for (let i = 0; i < s.length; i++) {
+        addHash(hashS, s[i])
+        addHash(hashT, t[i])
+    }
+
+    const sortedS = sortedHash(hashS)
+    const sortedT = sortedHash(hashT)
+
+    console.log(sortedS)
+    console.log(sortedT)
+
+    for (let i = 0; i < sortedS.length; i++) {
+        if ((sortedS[i][0] !== sortedT[i][0]) 
+        || (sortedS[i][1] !== sortedT[i][1])
+        ) {
+            return false
+        }
+    }
+
+    return true
+};
+
+const addHash = (hash, value) => {
+    if (!hash[value]) {
+        hash[value] = 0
+    }
+
+    hash[value]++
+}
+
+const sortedHash = (hash) => {
+    const res = Object.entries(hash).sort((a, b) => {
+        console.log(a[0].charCodeAt(), b[0].charCodeAt())
+        return a[0].charCodeAt() - b[0].charCodeAt()
+    })
+
+    return res
+
+}
