95f1aefbfcf48a961860da087d58e102ef3dc886
Gyuwon Lee (ollie) <94846990+gyutato@users.noreply.github.com>
2024-10-07 11:34:52 +0900

    Time: 65 ms (36.85%), Space: 51.6 MB (62.17%) - LeetHub

---
+++ LeetCode/0169-majority-element/0169-majority-element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const hash = {}
+    let res = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (!hash[nums[i]]) {
+            hash[nums[i]] = 0
+        }
+        hash[nums[i]]++
+
+        if (hash[nums[i]] > nums.length / 2) {
+            res = nums[i]
+            break;
+        }
+    }
+
+    return res
+};
