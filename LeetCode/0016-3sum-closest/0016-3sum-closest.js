e5afa047b9f96a62661bb73b2a42cf38e8591632
Gyuwon Lee (ollie) <94846990+gyutato@users.noreply.github.com>
2024-10-09 22:55:15 +0900

    Time: 71 ms (55.66%), Space: 51.1 MB (43.51%) - LeetHub

--- LeetCode/0016-3sum-closest/0016-3sum-closest.js
+++ LeetCode/0016-3sum-closest/0016-3sum-closest.js
@@ -15,10 +15,16 @@ var threeSumClosest = function(nums, target) {
         // move the pointers
         while (j < k) {
             const currSum = nums[i] + nums[j] + nums[k]
+
+            // early return condition
+            if (currSum === target) return target;
+
             // update the closest value
             if (Math.abs(target - closest) > Math.abs(target - currSum)) closest = currSum
+
             // lower the sum if current sum is bigger than the target value
             if (currSum > target) k--;
+
             // else, update the left pointer
             else j++;
         }
