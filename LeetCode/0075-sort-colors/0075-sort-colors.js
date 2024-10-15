c02140c6c0b04387139e2bd6382ca8ee8bfe426d
Gyuwon Lee (ollie) <94846990+gyutato@users.noreply.github.com>
2024-10-15 21:51:06 +0900

    Time: 53 ms (59.9%), Space: 49 MB (65.76%) - LeetHub

--- LeetCode/0075-sort-colors/0075-sort-colors.js
+++ LeetCode/0075-sort-colors/0075-sort-colors.js
@@ -3,26 +3,26 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    // two pointers
-    // 배열의 첫 번째 요소부터 확인
-    // 정렬되지 않은 영역 중 최솟값 -> 정렬할 자리와 swap
-    for (let i = 0; i < nums.length - 1; i++) {
-        let min = nums[i]
-        let minIdx = i;
-        for (let j = i + 1; j < nums.length; j++) {
-            // 최솟값 업데이트
-            if (nums[j] < min) {
-                min = nums[j]
-                minIdx = j
-            }
+    let red = 0;
+    let blue = nums.length - 1;
+    let white = 0;
+    while (white <= blue) {
+        if (nums[white] === 0) {
+            swap(nums, white, red);
+            red++;
+            white++;
+        } else if (nums[white] === 1) {
+            white++;
+        } else {
+            swap(nums, white, blue);
+            blue--;
         }
-        swap(nums, i, minIdx)
-        console.log(nums)
     }
+
 };
 
 const swap = (arr, idx1, idx2) => {
     const tmp = arr[idx1]
     arr[idx1] = arr[idx2]
     arr[idx2] = tmp
-}
+}                   
