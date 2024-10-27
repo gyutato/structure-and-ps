a9960246a929a6ae4221ea25e2762c2ab06aae5e
Gyuwon Lee (ollie) <94846990+gyutato@users.noreply.github.com>
2024-10-27 23:53:00 +0900

    Time: 0 ms (100%), Space: 49 MB (46.05%) - LeetHub

---
+++ LeetCode/1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(node) {
+    if (!node) return 0;
+    let value = ""
+
+    while (node) {
+        value += node.val
+        node = node.next
+    }
+
+    let i = 0;
+    let result = 0;
+    while (i < value.length) {
+        result += (Math.pow(2, i) * value[value.length - 1 - i])
+        i++;
+    }
+
+    return result
+};
