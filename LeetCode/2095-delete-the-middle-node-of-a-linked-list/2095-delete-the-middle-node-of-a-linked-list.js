31c63c4a07735a65f3d2fd6f08dc65155bddabb4
Gyuwon Lee (ollie) <94846990+gyutato@users.noreply.github.com>
2024-12-23 15:17:58 +0900

    Time: 4 ms (48.34%), Space: 90.6 MB (50.83%) - LeetHub

--- LeetCode/2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
+++ LeetCode/2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -15,31 +15,32 @@
 // 빠져나오다가, Math.floor(n / 2) - 1 번째 node 를 만나면 ('만난다' 의 정의 필요 -> node.next 인지, current node 인지 등)
 // node.next = node.next.next
 
+/**
+ * 모범답안: fast and slow pointers
+ * 재귀를 사용해서 길이 정보를 미리 저장해둘 수도 있지만, 포인터를 두 개 사용해서 절반을 찾아낼 수도 있음.
+ * 1씩 이동하는 slow pointer 와, n씩 이동하는 fast pointer 를 활용하여 1/n 지점의 요소를 한 번의 순회로 찾아내는 방식
+ *
+ * 재귀의 경우, 스택 전체에 걸쳐 공유되어야 하는 정보 (전체 길이) 와 각 스택이 깨질 때 리턴되어야 하는 정보 (node) 가 다르면
+ * 재귀함수 밖에 변수를 선언해주어야 함 (아마도? 다소 지저분...)
+ */
+
 var deleteMiddle = function (head) {
-  // base case 1. 주어진 노드 자체가 없을 때
-  if (!head) return head;
-  if (!head.next) return null;
+  // base case
+  if (!head || !head.next) return null;
 
-  let totalLen = 0;
+  // 포인터 초기화
+  let fast = head.next;
+  let slow = head;
 
-  // 재귀함수 정의
-  const traverse = (node) => {
-    // 리스트 길이 및 현재 노드 정보 업데이트
-    totalLen = totalLen + 1;
-    const currLen = totalLen;
+  // 포인터 이동 조건: fast 가 2칸 이동 가능한 경우
+  while (fast.next && fast.next.next) {
+    // 포인터 이동
+    slow = slow.next;
+    fast = fast.next.next;
+  }
 
-    // base case 2. 마지막 노드일 때
-    if (!node.next) return;
-
-    traverse(node.next)
-
-    // 만약 현재 노드가 중간 노드 직전 노드라면, 중간 노드를 건너뛰고 next 연결
-    if (currLen === Math.floor(totalLen / 2)) {
-      node.next = node.next.next;
-    }
-  };
-
-  traverse(head)
+  // 반복문 탈출 = slow 가 Math.floor(half) 위치의 노드를 가리키는 중
+  slow.next = slow.next.next;
 
   return head;
 };
